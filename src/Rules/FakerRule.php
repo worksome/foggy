a69566cd0835159a36c1f8b685b1f1f94a435a7d
Oliver Nybroe <oliver@worksome.com>
2020-12-21 14:21:42 +0100

    Fix faker rule

--- src/Rules/FakerRule.php
+++ src/Rules/FakerRule.php
@@ -9,7 +9,7 @@ use Worksome\Foggy\Settings\Rule as SettingsRule;
 
 class FakerRule implements Rule
 {
-    protected static FakerGenerator $faker;
+    protected static ?FakerGenerator $faker = null;
 
     public static function faker(): FakerGenerator
     {
